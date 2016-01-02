1c498209e895a8ce0d57a31103f3e9937a3ce9fa
mcarton <cartonmartin+git@gmail.com>
2016-01-02 19:07:45 +0100

    Test the to_lisp function

--- tests/lisp.rs
+++ tests/lisp.rs
@@ -19,15 +19,30 @@ fn test_parser() {
     assert_eq!(parse(""), Err(ParseError::EOE));
 
     assert_eq!(parse("0."), Ok(Lit(0.)));
-    assert_eq!(parse("herbie0"), Ok(Ident(0)));
     assert_eq!(parse("herbie"), Ok(Ident(0)));
+    assert_eq!(parse("(+ 0. herbie1)"), Ok(Binary(BiAdd, box Lit(0.), box Ident(0))));
+
+    let valid = &[
+        ("0", Lit(0.)),
+        ("herbie0", Ident(0)),
+        ("(+ herbie0 herbie1)", Binary(BiAdd, box Ident(0), box Ident(1))),
+        ("(- herbie0)", Unary(UnNeg, box Ident(0))),
+        ("(- herbie0 herbie1)", Binary(BiSub, box Ident(0), box Ident(1))),
+        ("(cos 1)", Fun("cos".into(), vec![Lit(1.)])),
+        ("(cos herbie0)", Fun("cos".into(), vec![Ident(0)])),
+        ("(log1p (cos herbie0))", Fun("log1p".into(), vec![Fun("cos".into(), vec![Ident(0)])])),
+    ];
+
+    for &(s, ref e) in valid {
+        assert_eq!(parse(s).as_ref(), Ok(e));
+        assert_eq!(s, e.to_lisp("herbie"));
+    }
+
 
     assert_eq!(parse("(+ 0. 0.) foobar"), Err(ParseError::EOE));
     assert_eq!(parse("("), Err(ParseError::EOE));
     assert_eq!(parse("0.eee"), Err(ParseError::Float));
 
-    assert_eq!(parse("(+ 0. herbie1)"), Ok(Binary(BiAdd, box Lit(0.), box Ident(0))));
-    assert_eq!(parse("(+ herbie0 herbie1)"), Ok(Binary(BiAdd, box Ident(0), box Ident(1))));
 
     assert_eq!(parse("+"), Err(ParseError::Unexpected('+')));
     assert_eq!(parse("(+)"), Err(ParseError::Unexpected(')')));
@@ -36,16 +51,10 @@ fn test_parser() {
     assert_eq!(parse("(+ 0. 0. 0.)"), Err(ParseError::Expected(')')));
     assert_eq!(parse("(+ 0. 0. 0."), Err(ParseError::Expected(')')));
 
-    assert_eq!(parse("(- herbie0)"), Ok(Unary(UnNeg, box Ident(0))));
-    assert_eq!(parse("(- herbie0 herbie1)"), Ok(Binary(BiSub, box Ident(0), box Ident(1))));
-
     assert_eq!(parse("-"), Err(ParseError::Unexpected('-')));
     assert_eq!(parse("(-)"), Err(ParseError::Unexpected(')')));
     assert_eq!(parse("(- 0. 0. 0.)"), Err(ParseError::Expected(')')));
 
-    assert_eq!(parse("(cos 1.)"), Ok(Fun("cos".into(), vec![Lit(1.)])));
-    assert_eq!(parse("(cos herbie0)"), Ok(Fun("cos".into(), vec![Ident(0)])));
-    assert_eq!(parse("(log1p (cos herbie0))"), Ok(Fun("log1p".into(), vec![Fun("cos".into(), vec![Ident(0)])])));
 
     assert_eq!(parse("(cos)"), Err(ParseError::Arity));
     assert_eq!(parse("(cos"), Err(ParseError::Expected(')')));
